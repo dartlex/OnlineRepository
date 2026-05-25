16d731ca0a16d6bd19c320d01f99849aca9bbc41
dartlex <dartlex.rmnv@gmail.com>
2026-05-25 14:22:59 +0600

    Создал проект, добавил строчки кода

---
+++ OnlineRepository/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineRepository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Я что-то изменил в программе");
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
